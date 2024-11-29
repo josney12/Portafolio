4ed3d4eaea726ff6386ede9b13a3db99f8b5162b
josney castillo <josneycastillo2011@gmail.com>
2024-11-29 16:00:11 -0500

    Add files via upload

---
+++ portfolio_V1-main/portfolio_V1-main/script.js
@@ -0,0 +1,78 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("wordpress");
+        habilidades[5].classList.add("proyect");
+        habilidades[6].classList.add("proyect");
+        habilidades[7].classList.add("proyect");
+        habilidades[8].classList.add("proyect");
+        habilidades[9].classList.add("proyect");
+        habilidades[10].classList.add("proyect");
+        habilidades[11].classList.add("proyect");
+        habilidades[12].classList.add("proyect");
+        habilidades[13].classList.add("proyect");
+        habilidades[14].classList.add("proyect");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
+
+
+// function toggleTheme() {
+//     const body = document.body;
+//     const themeIcon = document.getElementById('themeIcon');
+
+//     body.classList.toggle('light-mode');
+
+//     if (body.classList.contains('light-mode')) {
+//         themeIcon.classList.remove('fa-sun');
+//         themeIcon.classList.add('fa-moon');
+//     } else {
+//         themeIcon.classList.remove('fa-moon');
+//         themeIcon.classList.add('fa-sun');
+//     }
+// }
+
+function toggleTheme() {
+    const body = document.body;
+    const themeIcon = document.getElementById('themeIcon');
+    const imgElement = document.querySelector('.img_Card');
+
+    body.classList.toggle('light-mode');
+
+    if (!body.classList.contains('light-mode')) {
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
+    } else {
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
+    }
+}
